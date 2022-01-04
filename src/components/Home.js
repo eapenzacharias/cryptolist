c448a191832bf855bd9c7381a9a15fd56d4b6b43
eapenzacharias <eapenzac@gmail.com>
2022-01-04 17:30:45 +0100

    added react-bbotstarp

---
+++ src/components/Home.js
@@ -0,0 +1,17 @@
+const Home = () => {
+  const UnderConstruction = () => (
+    <>
+      <div>
+        <h1>The site is Under Construction</h1>
+      </div>
+    </>
+  );
+
+  return (
+    <>
+      <UnderConstruction />
+    </>
+  );
+};
+
+export default Home;
---
+++ src/components/NavBar.js
@@ -0,0 +1,17 @@
+const NavBar = () => {
+  const UnderConstruction = () => (
+    <>
+      <div>
+        <h1>The site is Under Construction</h1>
+      </div>
+    </>
+  );
+
+  return (
+    <>
+      <UnderConstruction />
+    </>
+  );
+};
+
+export default NavBar;
